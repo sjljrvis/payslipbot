2eb1d85f29cb07b3b33c0ee535c53168618f9f09
sjjrvis <sjlchougule@gmail.com>
2018-02-05 00:25:15 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+exports.port = process.env.PORT || 3000;
+
+exports.mongodb = {
+	uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://sejal:sejal@ds121268.mlab.com:21268/slackbot'
+};
+
+exports.mailDetails = {
+	host: "smtp.gmail.com",
+	port: 465,
+	user: "sejal@binarynumbers.io",
+	password: "jarvis1995"
+}
+
+exports.slack = {
+	botToken: 'xoxb-309498882962-hHWpQbmTbgQCjYpVDvURt7Om'
+}
+
+exports.apiaiAuth = {
+	token: 'cd80027eb15f48d49b05cfef580f2aae'
+}
